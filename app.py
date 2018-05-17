4ff88a96908c553506cb8eba8637030c55ceb896
HP Nunes <hp.nunes@protonmail.com>
2018-05-17 14:49:46 -0800

    made updates to analysis and app.py

--- app.py
+++ app.py
@@ -20,51 +20,73 @@ app = Flask(__name__)
 #################################################
 
 @app.route("/api/v1.0/precipitation")
-def names():
-    """Return a list of all passenger names"""
-    # Query all passengers
-    results = session.query(Passenger.name).all()
+def prcp_data():
+    """Return precipitation data"""
+    # Query for the dates and temperature observations from the last year.
+    precip_last_year = dt.date.today()-dt.timedelta(days=365)
+    
+    prcp_data = session.query(Measurement.date,Measurement.prcp).\
+        filter(Measurement.date >= precip_last_year).all()
+    
+    # Convert the query results to a Dictionary using date as the key and tobs as the value.
+    prcp_json = {date: prcp for date, prc in prcp_data}
+    # Return the json representation of your dictionary.
+    return jsonify(prcp_json)
+
+@app.route("/api/v1.0/stations")
+def stations_data():
+    """Return all stations"""
+    # Return a json list of stations from the dataset.
+    station_data = session.query(Station.station).all()
 
     # Convert list of tuples into normal list
-    all_names = list(np.ravel(results))
+    stations_json = list(np.ravel(station_data))
 
-    return jsonify(all_names)
-
+    return jsonify(stations_json)
 
 @app.route("/api/v1.0/tobs")
-def passengers():
-    """Return a list of passenger data including the name, age, and sex of each passenger"""
-    # Query all passengers
-    results = session.query(Passenger).all()
+def temp_data():
+    """Return temperature data for the last year"""
+    # Return a json list of Temperature Observations (tobs) for the previous year
+    precip_last_year = dt.date.today()-dt.timedelta(days=365)
+    
+    temp_data = session.query(Measurement.date,Measurement.tobs).\
+        filter(Measurement.date >= precip_last_year).all()
+        
+    temp_json = {date: tobs for date, tobs in temp_data}
 
-    # Create a dictionary from the row data and append to a list of all_passengers
-    all_passengers = []
-    for passenger in results:
-        passenger_dict = {}
-        passenger_dict["name"] = passenger.name
-        passenger_dict["age"] = passenger.age
-        passenger_dict["sex"] = passenger.sex
-        all_passengers.append(passenger_dict)
-
-    return jsonify(all_passengers)
+    return jsonify(temp_json)
 
 @app.route("/api/v1.0/<start>")
-def welcome():
-    """List all available api routes."""
-    return (
-        f"Available Routes:<br/>"
-        f"/api/v1.0/names<br/>"
-        f"/api/v1.0/passengers"
-    )
+def start_temp_stats():
+    """Return a json list of the minimum temperature, the average temperature, and the max temperature for a given start or start-end range"""
+     """When given the start only, calculate TMIN, TAVG, and TMAX for all dates greater than and equal to the start date."""
+
+    start_date_entry = input('Enter a start date in YYYY-MM-DD format')
+    year, month, day = map(int, date_entry.split('-'))
+    start_date = datetime.date(year, month, day)
+    
+    query = session.query(Measurement, Station).join(Station)
+    # https://stackoverflow.com/questions/6044309/sqlalchemy-how-to-join-several-tables-by-one-query
+    
+    start_temp_data = session.query(Measurement.date,Measurement.tobs).\
+        filter(Measurement.date >= start_date).all()
+        
+    TMIN = start_temp_data.min().all()
+    TAVG = start_temp_data.mean().all()
+    TMAX = start_temp_data.max().all()
+    
+    start_temp_stats_json = {date: TMIN : TAVG: TMAX for date in start_temp_data}
+
+    return jsonify()
+
 
 @app.route("/api/v1.0/<start>/<end>")
-def welcome():
-    """List all available api routes."""
-    return (
-        f"Available Routes:<br/>"
-        f"/api/v1.0/names<br/>"
-        f"/api/v1.0/passengers"
-    )
+def start_end_temp_stats():
+    """When given the start and the end date, calculate the TMIN, TAVG, and TMAX for dates between the start and end date inclusive."""
+
+
+    return jsonify()
 
 if __name__ == '__main__':
     app.run(debug=True)
